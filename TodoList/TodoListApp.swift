05cb8ff414a29fb87b35441c86cc4e4893ffd2bc
Abrar <abraralaq@gmail.com>
2021-06-15 21:26:03 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by ابرار on 05/11/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
